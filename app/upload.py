65ecf29439e45a0aeaf4d3913ffad4b303be4293
not VFLC <radioactive-lollipop@proton.me>
2024-01-13 00:18:53 +0800

    generate preview for .bmp files

--- app/upload.py
+++ app/upload.py
@@ -9,6 +9,21 @@ from PIL import Image, UnidentifiedImageError
 
 from app import config
 
+
+class MyException(Exception):
+    message: str
+
+    def __init__(self, message, *args, **kwargs):
+        super().__init__(message, *args, **kwargs)
+        self.message = message
+
+class NonCriticalException(MyException):
+    pass
+
+class CriticalException(MyException):
+    pass
+
+
 class UploadExtension(interactions.Extension):
     bot: interactions.Client
     config = config.Config()
@@ -41,7 +56,7 @@ class UploadExtension(interactions.Extension):
             ),
             interactions.SlashCommandOption(
                 name="fulfilled_request_link",
-                description="Paste the link to the fulfilled request here",
+                description="Paste the link (or id) to the fulfilled request here",
                 required=False,
                 type=interactions.OptionType.STRING,
             ),
@@ -54,11 +69,29 @@ class UploadExtension(interactions.Extension):
         image_attachment: interactions.Attachment,
         fulfilled_request_link: typing.Optional[str] = None,
     ):
+        try:
+            await self._upload_anonymously(ctx, upload_channel, image_attachment, fulfilled_request_link)
+        except CriticalException as e:
+            await ctx.send(f"**ERROR**\n{e.message}", ephemeral=True)
+        except:
+            await ctx.send(
+                "**UNEXPEDTED ERROR**\n" +
+                "Something went wrong. Please try /upload again later.",
+                ephemeral=True
+            )
+
+    async def _upload_anonymously(
+        self,
+        ctx: interactions.SlashContext,
+        upload_channel: str,
+        image_attachment: interactions.Attachment,
+        fulfilled_request_link: typing.Optional[str] = None,
+    ):
         # form handler
-        upload_form = self.get_description_form_modal()
+        upload_form = self.generate_form()
         await ctx.send_modal(modal=upload_form)
         upload_form_ctx = await ctx.bot.wait_for_modal(upload_form, author=ctx.author)
-        form_loading_message = await upload_form_ctx.send("Anonahira is processing your post...", ephemeral=True)
+        form_loading_message = await upload_form_ctx.send("processing your post...", ephemeral=True)
 
         # build and publish anonymous post
         description = upload_form_ctx.responses["description"]
@@ -69,43 +102,28 @@ class UploadExtension(interactions.Extension):
         if fulfilled_request_link:
             try:
                 users_to_ping = await self.fetch_requested_users(fulfilled_request_link)
+            except NonCriticalException as e:
+                await ctx.send(e.message, ephemeral=True)
             except:
-                await ctx.send("Invalid fulfilled request link provided, will skip pinging users.", ephemeral=True)
-                return
+                await ctx.send(
+                    "You provided a link to the request you fulfilled; however, something went wrong when parsing the link.\n"
+                    "The upload process will continue without pinging anyone",
+                    ephemeral=True
+                )
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(image_attachment.url) as res:
-                if res.status != 200:
-                    raise Exception("Failed to download image")
-                original_image_raw = await res.read()
-
-        image_files = []
-        original_image = Image.new("L", (1, 1))
-        original_image_filename = image_attachment.filename
+        image_files: list[interactions.File] = list()
         try:
-            original_image = Image.open(io.BytesIO(original_image_raw))
-            if original_image.format in ["BMP"]:
-                with tempfile.NamedTemporaryFile(delete=False) as f:
-                    original_image.save(f.name, format="PNG")
-                    preveiw_file = interactions.File(
-                        file=f.name,
-                        file_name="generated_preview.png",
-                        content_type="image/png",
-                    )
-                    image_files.append(preveiw_file)
-                    original_image_filename = f"original_image_file - {image_attachment.filename}"
-        except UnidentifiedImageError:
-            original_image_filename = f"no_preview - {image_attachment.filename}"
+            await self.fetch_images(image_attachment, image_files)
+        except NonCriticalException as e:
+            await ctx.send(e.message, ephemeral=True)
+        except CriticalException as e:
+            return await ctx.send(e.message, ephemeral=True)
+        except:
+            raise CriticalException("Something went wrong while processing the image")
 
-        original_file = interactions.File(
-            file=io.BytesIO(original_image_raw),
-            file_name=original_image_filename,
-            content_type=image_attachment.content_type,
-        )
-        image_files.append(original_file)
-
+        message = f"{description}\n{users_to_ping}".strip()
         anonymous_post_message = await target_channel.send(
-            content=f"{description}\n{users_to_ping}",
+            content=message,
             files=image_files,
             suppress_embeds=True,
         )
@@ -118,24 +136,26 @@ class UploadExtension(interactions.Extension):
 
         # send confirmation message
         await ctx.send(
-            "The post has been uploaded to the server.\n" +
-                f"Link: {anonymous_post_message.jump_url}",
+            "Your post has been submitted. Thank you for sharing!\n"
+            f"Link: {anonymous_post_message.jump_url}",
             ephemeral=True,
-            components=[interactions.ActionRow(
-                interactions.Button(
-                    style=interactions.ButtonStyle.BLURPLE,
-                    label="Edit Description",
-                    custom_id="btn:edit_description",
+            components=[
+                interactions.ActionRow(
+                    interactions.Button(
+                        style=interactions.ButtonStyle.BLURPLE,
+                        label="Edit Description",
+                        custom_id="btn:edit_description",
+                    ),
+                    interactions.Button(
+                        style=interactions.ButtonStyle.RED,
+                        label="Delete Post",
+                        custom_id="btn:delete_post",
+                    )
                 ),
-                interactions.Button(
-                    style=interactions.ButtonStyle.RED,
-                    label="Delete Post",
-                    custom_id="btn:delete_post",
-                )
-            )],
+            ]
         )
 
-    def get_description_form_modal(self, value=""):
+    def generate_form(self, value=""):
         return interactions.Modal(
             interactions.ParagraphText(
                 label="Description",
@@ -148,48 +168,54 @@ class UploadExtension(interactions.Extension):
         )
 
     async def get_anonymous_post(self, confirmation_callback_ctx: interactions.ComponentContext):
-        """Returns `None` if unexpected error happened, `str` if expected error, `Message` if success."""
         message = confirmation_callback_ctx.message
         if not message:
-            return None
+            raise CriticalException("Failed to load the confirmation message")
+
         searchResult = re.search(r"(?P<url>https?://[^\s]+)", message.content)
         if not searchResult:
-            return None
+            raise CriticalException("Failed to extract link from confirmation message")
+
         link = searchResult.group("url")
         if not isinstance(link, str):
-            return None
+            raise CriticalException("Failed to extract link from confirmation message")
 
-        channel_id = int(link.split("/")[-2])
-        message_id = int(link.split("/")[-1])
+        try:
+            channel_id = int(link.split("/")[-2])
+            message_id = int(link.split("/")[-1])
+        except:
+            raise CriticalException("Loaded confirmation message, but failed to parse it")
 
         anonymous_post_message = await self.config.channels[channel_id].fetch_message(message_id)
         if not anonymous_post_message:
-            return "Post not found, it may have already been deleted by admins.",
+            raise CriticalException("Could not find your post; it may have been deleted by admins.")
         return anonymous_post_message
 
     @interactions.component_callback("btn:delete_post")
     async def delete_post(self, ctx: interactions.ComponentContext):
-        anonymous_post_message = await self.get_anonymous_post(ctx)
-        if isinstance(anonymous_post_message, str):
-            return await ctx.send(anonymous_post_message , ephemeral=True)
-        if not isinstance(anonymous_post_message, interactions.Message):
+        try:
+            anonymous_post_message = await self.get_anonymous_post(ctx)
+        except CriticalException as e:
+            return await ctx.send(e.message, ephemeral=True)
+        except:
             return await ctx.send("An execpected error happened.", ephemeral=True)
 
         await anonymous_post_message.delete(context=ctx)
-        await ctx.send("Post deleted.", ephemeral=True)
+        await ctx.send("Your post has been deleted", ephemeral=True)
         if ctx.message:
             await ctx.message.delete(context=ctx)
 
     @interactions.component_callback("btn:edit_description")
     async def edit_discription(self, ctx: interactions.ComponentContext):
-        anonymous_post_message = await self.get_anonymous_post(ctx)
-        if isinstance(anonymous_post_message, str):
-            return await ctx.send(anonymous_post_message , ephemeral=True)
-        if not isinstance(anonymous_post_message, interactions.Message):
+        try:
+            anonymous_post_message = await self.get_anonymous_post(ctx)
+        except CriticalException as e:
+            return await ctx.send(e.message, ephemeral=True)
+        except:
             return await ctx.send("An execpected error happened.", ephemeral=True)
 
         # form handler
-        form_modal = self.get_description_form_modal(anonymous_post_message.content)
+        form_modal = self.generate_form(anonymous_post_message.content)
         await ctx.send_modal(modal=form_modal)
         form_ctx = await ctx.bot.wait_for_modal(
             form_modal,
@@ -199,20 +225,78 @@ class UploadExtension(interactions.Extension):
         # update description
         description = form_ctx.responses["description"]
         await anonymous_post_message.edit(content=description)
-        await form_ctx.send("Description updated.", ephemeral=True)
+        await form_ctx.send("Description has been updated", ephemeral=True)
 
     async def fetch_requested_users(self, fulfilled_request_link: str) -> str:
-        request_message_id = int(fulfilled_request_link.split("/")[-1])
+        try:
+            request_message_id = int(fulfilled_request_link.split("/")[-1])
+        except:
+            raise NonCriticalException(
+                "You provided a link to the request you fulfilled; however, something went wrong while parsing the link.\n"
+                "The upload process will continue without pinging anyone"
+            )
+
         request_message = await self.config.upload_request_channel.fetch_message(request_message_id)
         if request_message is None:
-            raise Exception("Cannot find message")
+            raise NonCriticalException(
+                "You provided a link to the request you fulfilled; however, the message could not be found.\n"
+                "The upload process will continue without pinging anyone"
+            )
 
         pingme_reaction = next((reaction for reaction in request_message.reactions if reaction.emoji.name == "pingme"), None)
         if pingme_reaction is None:
-            raise Exception("No one reacted to your message with :pingme:")
+            raise NonCriticalException(
+                "You provided a link to the fulfilled request, but no one reacted to the message with a :pingme: emoji.\n"
+                "The upload process will continue without pinging anyone"
+            )
 
         users = await pingme_reaction.users().fetch()
         return "".join([u.mention for u in users])
+
+    async def fetch_images(
+        self,
+        image_attachment: interactions.Attachment,
+        image_files: list[interactions.File]
+    ):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(image_attachment.url) as res:
+                if res.status != 200:
+                    raise CriticalException(
+                        "Failed to download the image you sent.\n"
+                        "This shouldn't happend, try /upload again and see if it helps."
+                    )
+                original_image_raw = await res.read()
+
+        original_image_filename = image_attachment.filename
+        original_file = interactions.File(
+            file=io.BytesIO(original_image_raw),
+            file_name=original_image_filename,
+            content_type=image_attachment.content_type,
+        )
+        image_files.append(original_file)
+
+        try:
+            original_image = Image.open(io.BytesIO(original_image_raw))
+            if original_image.format in ["BMP"]:
+                with tempfile.NamedTemporaryFile(delete=False) as f:
+                    original_image.save(f.name, format="PNG")
+                    preview_file = interactions.File(
+                        file=f.name,
+                        file_name="anonahira_generated_preview.png",
+                        content_type="image/png",
+                    )
+                    image_files[0].file_name = f"original cover - {image_attachment.filename}"
+                    image_files.insert(0, preview_file)
+        except UnidentifiedImageError:
+            raise NonCriticalException(
+                "The file you uploaded is not recognized as an image.\n"
+                "The upload process will continue but you might not see a preview image for your post."
+            )
+        except:
+            raise NonCriticalException(
+                "Discord does not support the format of your image, and something went wrong while generating a preview image for it.\n"
+                "The upload process will continue but you might not see a preview image for your post."
+            )
 
 
 def setup(bot: interactions.Client):
